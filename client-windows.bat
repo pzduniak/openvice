a37837ab36196a31abc0ebaae53331a6ae331d67
Piotr Zduniak <piotr@zduniak.net>
2013-11-03 22:58:59 +0100

    Fixed client-windows.bat

--- client-windows.bat
+++ client-windows.bat
@@ -1,7 +1,9 @@
+mkdir output
+
 utility\wget https://openvice.net/bases/openvice-base-1.zip
 utility\unzip openvice-base-1.zip -d output
 
 copy "release\Multi Theft Auto.exe" "output\OpenVice.exe"
 copy "release\MTA\*.dll" "output\MTA"
 copy "release\mods\deathmatch\*.dll" "output\mods\deathmatch"
-
+copy "release\server" "output\server"
