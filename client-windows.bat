aac6ddad67733cce60760aaec403c84e16907549
Piotr Zduniak <piotr@zduniak.net>
2013-11-03 22:57:43 +0100

    Added a build script

---
+++ client-windows.bat
@@ -0,0 +1,7 @@
+utility\wget https://openvice.net/bases/openvice-base-1.zip
+utility\unzip openvice-base-1.zip -d output
+
+copy "release\Multi Theft Auto.exe" "output\OpenVice.exe"
+copy "release\MTA\*.dll" "output\MTA"
+copy "release\mods\deathmatch\*.dll" "output\mods\deathmatch"
+
