0bb478daabd306cad615196f8cb8c528f22458e4
Piotr Zduniak <piotr@zduniak.net>
2013-11-04 19:42:55 +0100

    Turned downloads on

--- client-windows.bat
+++ client-windows.bat
@@ -5,7 +5,7 @@ mkdir output\server
 mkdir output\server\mods
 mkdir output\server\mods\deathmatch
 
-::utility\wget https://openvice.net/bases/openvice-base-1.zip
+utility\wget https://openvice.net/bases/openvice-base-1.zip
 utility\unzip openvice-base-1.zip -d output
 
 copy "release\Multi Theft Auto.exe" "output\OpenVice.exe"
